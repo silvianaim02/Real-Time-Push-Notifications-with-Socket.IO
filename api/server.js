ea1b18fa89a014127b4f4a313cbffea62c1103b7
Silvia Naim <silvia.naim@dansmultipro.com>
2024-09-30 14:37:29 +0700

    feat: create server api

---
+++ api/server.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketIO(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors('*'));
+
+app.use(express.json());
+
+app.post('/send', (req, res) => {
+  const message = req.body.message;
+  console.log(message);
+
+  io.emit('pushNotification', { message });
+  res.status(200).send({ message: 'Message sent successfully' });
+
+  io.on('connection', (socket) => {
+    console.log('Connected');
+    socket.on('disconnect', () => {
+      console.log('Client Disconnected');
+    });
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
